42b6738fe0c8e21681776f32392272755863b025
sum1190 <sum1190@gmail.com>
2013-10-07 18:30:03 +0200

    modifica

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <html>
 <head>
-<title></title>
-
+	<link rel="stylesheet" href="./css/main.css" type="text/css"/>
+<title>School Project Login</title>
 </head>
 
 <body>
-index
+
+
+<div id="wrapper">
+	<div id="header">Nome Sito</div>
+	<div id="container">
+		<div id="content">cont</div>
+	</div>
+</div>
+	
+
 </body>
 </html>
