80d4f9fa59326f1114d75bc2d5102ac2ec59865a
sum1190 <sum1190@gmail.com>
2013-10-08 10:09:03 +0200

    refreshed stylesheet new index based on new css

--- index.php
+++ index.php
@@ -6,15 +6,18 @@
 
 <body>
 
-
+<!--
 <div id="wrapper">
 	<div id="header">
 		
 	<img src="./img/logo.png" alt="logo" width="80" height="80" style="float:left;" />
 	<h3 id="headertext" >School Project</h3>
 	</div>
-
-		<div id="middle">
+	<div id="container">
+		<div id="content">
+			<div id="sideLeft">flefa</div>
+			
+			<div id="middle">
 			<br><br><br>
 			<form name="login" method="post" id="loginform">
 				<input id="logininput" type="text" value="User" name="tUser" />
@@ -27,8 +30,21 @@
 
 			</form>
 	</div>
-</div>
+		</div>
+	</div>
 	
 
+</body>
+-->
+
+<body>
+	<div id="header">
+		<img src="./img/logo.png" alt="logo" width="80" height="80" style="float:left;" />
+		<h1 align="center" id="headertext" >School Project</h1>
+	</div>
+	
+	<div id="leftcol">gianni lol</div>
+	
+	<div id="maincol">gianni malacarne</div>
 </body>
 </html>
