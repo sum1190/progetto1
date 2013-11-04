1a72af5ab2c9e3694a5fe7354a5fa279ace2b37f
unknown <giocus@hotmail.it>
2013-11-04 21:13:08 +0100

    inseriti i link ai pc nel menu quindi ho modificato utils

--- src/utils.php
+++ src/utils.php
@@ -9,14 +9,42 @@
 	function controllo($valore_pagina){
 		switch($_GET['action'])
 		{
-			case 'info':	info();		break;
+			case 'info':	//Stampa le informazioni del sito 
+							info();		break;
+			
+			case 'pc'  :	//Stampa le informazioni su un determinato pc
+							$npc=$_GET['npc'];
+							if($npc){
+								//controllo se il pc esiste e appartiene all'utente
+								$o = new database();
+								//Labb32_pc1
+								$qsql="select * from pc,laboratori where nomepc='".$npc."' and pc.idlab=laboratori.idlab and laboratori.iduser=".$_SESSION['id_user']."; ";
+								$o->exec_query($qsql,array());
+								$nr=$o->qry_size();
+								$qr=$o->get_qry_rows();
+								//stampo le informazioni sul pc selezionato
+								
+								if($nr>0){
+									print_r($qr);
+								}
+							}			
+							break;
 			default:
                 echo '';
 		}	
 	}
 	
 	
-	//Crea dinamicamente il bottore scuola
+	
+	
+	
+	
+	
+	
+	
+	//Crea dinamicamente il bottore scuola.
+	//Dato l'id dell'utente restituisce i dati relativi alle scuole , laboratori e pc 
+	//di cui è responsabile.
 	function scuole($id){
 		$o = new database();
 		//Prendo il numero di laboratori per scuola 
@@ -24,9 +52,6 @@
 		$o->exec_query($qsql1,array());
 		$num_lab=$o->qry_size();
 		$vet_num_lab=$o->get_qry_rows();
-	
-	
-		
 		//$qsql="SELECT nomescuola, idlab FROM scuola, regist, laboratori WHERE regist.iduser =".$id." AND scuola.idistituto = regist.idistituto AND scuola.idistituto = laboratori.idistituto";
 		//$qsql="SELECT nomescuola, pc.idlab ,nomepc FROM scuola, regist, laboratori ,pc WHERE regist.iduser =".$id." AND scuola.idistituto = regist.idistituto AND scuola.idistituto = laboratori.idistituto AND pc.idlab = laboratori.idlab";
 		$qsql="SELECT nomescuola, pc.idlab, nomepc FROM scuola, regist, laboratori, pc WHERE regist.iduser =".$id." AND scuola.idistituto = regist.idistituto AND scuola.idistituto = laboratori.idistituto AND pc.idlab = laboratori.idlab AND laboratori.iduser=".$id."";
@@ -52,7 +77,7 @@
 									echo"<ul>";
 									for($z=0; $z<$n; $z++){//echo"<li><a>pc1</a>";
 										if(strcmp($rw[$j][1],$rw[$z][1])==0){
-											echo "<li><a>".$rw[$z][2]."</a></li>";		//stampo il laboratorio
+											echo " <li><a href=\"logged.php?action=pc&npc=".$rw[$z][2]." \" >".$rw[$z][2]."</a></li>";		//stampo il pc
 										}
 									}
 									echo"</ul>";
